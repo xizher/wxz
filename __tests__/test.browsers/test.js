384a74f56ef085a7756fc406ac0447ead576fef7
xizher <xizher@163.com>
2021-01-17 14:56:34 +0800

    test: test with vue

--- __tests__/test.browsers/test.js
+++ __tests__/test.browsers/test.js
@@ -1,3 +1,6 @@
+import { reactive, ref, toRef, watch } from 'vue'
+import { LinkedList } from '../../src/data-structure/linked-list/linked-list'
+import LinkedListNode from '../../src/data-structure/linked-list/linked-list-node'
 import {
   BaseUtils
 } from '../../src/js-utils'
@@ -8,24 +11,54 @@ window.onload = () => {
     log(BaseUtils.getRootPath())
   }
   {
-    BaseUtils.getGeoLocation().then(res => {
-      log(res)
-    })
+    // BaseUtils.getGeoLocation().then(res => {
+    //   log(res)
+    // })
   }
   {
-    const removeEvent = BaseUtils.watchGeoLocation(event => {
-      log(event)
-    })
-    setTimeout(() => {
-      removeEvent.remove()
-    }, 1000)
+    // const removeEvent = BaseUtils.watchGeoLocation(event => {
+    //   log(event)
+    // })
+    // setTimeout(() => {
+    //   removeEvent.remove()
+    // }, 1000)
   }
   {
     log(BaseUtils.isFromMobileBrowser())
   }
   {
-    BaseUtils.copyText('copy hhh').then(res => {
-      console.log(res)
-    })
+    // BaseUtils.copyText('copy hhh').then(res => {
+    //   console.log(res)
+    // })
+  }
+  {
+    // const list = new LinkedList()
+    // const vlist = reactive(list)
+    // watch(toRef(vlist, 'length'), val => {
+    //   console.log(val)
+    // })
+    // list.append('00')
+    // vlist.value.append('00')
+    // console.log(vlist)
+
+    const node = reactive(new LinkedListNode(1))
+    // const node = new LinkedListNode(1)
+
+    watch(node, val => {
+      console.log(val.value)
+    }, { deep: true })
+    node.value = 2
+
+    console.log(node.value)
+
+    // const test = reactive({
+    //   a: 1
+    // })
+    // watch(toRef(test, 'a'), val => {
+    //   console.log(val)
+    // })
+    // test.a++
+    
+
   }
 }
