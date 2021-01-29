6612eedd849eb98c7fcf66dd5dcd47394de7ef22
xizher <xizher@163.com>
2021-01-29 22:33:48 +0800

    feat: extent对象扩展 -> 获取四角的点对象以及转多边形对象的方法

--- src/gis/esri/esri-exts/esri-exts.d.ts
+++ src/gis/esri/esri-exts/esri-exts.d.ts
@@ -5,6 +5,11 @@ export declare function registerEsriExts (map: $Map) : (view: $View) => void
 export declare interface IEsriExtExtent {
   getMaxLonLat () : [number, number]
   getMinLonLat () : [number, number]
+  getNorthEastPoint() : __esri.Point
+  getNorthWestPoint() : __esri.Point
+  getSouthWestPoint() : __esri.Point
+  getSouthEastPoint() : __esri.Point
+  toPolygon () : __esri.Polygon
 }
 
 export declare function $esriExt (_this: __esri.Extent) : IEsriExtExtent
--- src/gis/esri/esri-exts/esri-exts.js
+++ src/gis/esri/esri-exts/esri-exts.js
@@ -1,4 +1,5 @@
 import { esri } from '../esri-modules/esri-modules'
+import { EsriUtils } from '../esri-utils/esri-utils'
 
 /** @type {import('../webmap/webmap').$Map} */
 let _map = null
@@ -14,21 +15,58 @@ export function $esriExt (_this) {
   if (_this instanceof esri.geometry.Extent) {
     return {
       getMaxLonLat () {
+        const point = $esriExt(_this).getNorthEastPoint()
+        return [point.longitude, point.latitude]
+      },
+      getMinLonLat () {
+        const point = $esriExt(_this).getSouthWestPoint()
+        return [point.longitude, point.latitude]
+      },
+      getNorthEastPoint () {
         const { xmax, ymax } = _this
         const { spatialReference } = _view
         const point = new esri.geometry.Point({
           x: xmax, y: ymax, spatialReference
         })
-        return [point.longitude, point.latitude]
+        return point
       },
-      getMinLonLat () {
+      getNorthWestPoint () {
+        const { xmin, ymax } = _this
+        const { spatialReference } = _view
+        const point = new esri.geometry.Point({
+          x: xmin, y: ymax, spatialReference
+        })
+        return point
+      },
+      getSouthWestPoint () {
         const { xmin, ymin } = _this
         const { spatialReference } = _view
         const point = new esri.geometry.Point({
           x: xmin, y: ymin, spatialReference
         })
-        return [point.longitude, point.latitude]
-      }
+        return point
+      },
+      getSouthEastPoint () {
+        const { xmax, ymin } = _this
+        const { spatialReference } = _view
+        const point = new esri.geometry.Point({
+          x: xmax, y: ymin, spatialReference
+        })
+        return point
+      },
+      toPolygon () {
+        const extentExt = $esriExt(_this)
+        const northEast = extentExt.getNorthEastPoint()
+        const northWest = extentExt.getNorthWestPoint()
+        const southWest = extentExt.getSouthWestPoint()
+        const southEast = extentExt.getSouthEastPoint()
+        return EsriUtils.pointsToPolygon([
+          northEast,
+          northWest,
+          southWest,
+          southEast,
+        ])
+      },
     }
   }
 }
