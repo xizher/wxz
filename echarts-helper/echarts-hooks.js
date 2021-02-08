79aaeb6945fbb695b78d2d64d66bd1ccbe706dff
xizher <xizher@163.com>
2021-02-08 16:31:10 +0800

    feat: 配置项默认值

--- echarts-helper/echarts-hooks.js
+++ echarts-helper/echarts-hooks.js
@@ -1,7 +1,7 @@
 import { onMounted } from 'vue'
 import { EChartsHelper } from './echarts-hepler'
 
-export function useECharts (divId, options) {
+export function useECharts (divId, options = {}) {
   const echartsHelper = new EChartsHelper(divId)
   onMounted(() => echartsHelper.mount().setOptions(options))
   return [
